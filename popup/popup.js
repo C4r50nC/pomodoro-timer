4d890eb4754e55cae49054da75e9c551e3b0d465
C4r50nC <xichen059@gmail.com>
2024-03-17 10:30:02 +1100

    feat: implement task creation

---
+++ popup/popup.js
@@ -0,0 +1,29 @@
+const tasks = [];
+
+const addTaskBtn = document.getElementById("add-task-btn");
+addTaskBtn.addEventListener("click", () => addTaskBtn());
+
+function addTask() {
+  const tasksNum = tasks.length;
+  tasks.push("");
+
+  const taskRow = document.createElement("div");
+
+  const taskName = document.createElement("input");
+  taskName.type = "text";
+  taskName.placeholder = "Enter a task...";
+  taskName.addEventListener("change", () => {
+    tasks[tasksNum] = taskName.value;
+    console.log(tasks);
+  });
+
+  const deleteBtn = document.createElement("input");
+  deleteBtn.type = "button";
+  deleteBtn.value = "X";
+
+  taskRow.appendChild(taskName);
+  taskRow.appendChild(deleteBtn);
+
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.appendChild(taskRow);
+}
