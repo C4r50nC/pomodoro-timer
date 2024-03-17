0fa4c203d9845e26f39acf848f1a0b793be64c66
C4r50nC <xichen059@gmail.com>
2024-03-17 12:55:39 +1100

    feat: add timer notification

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
+const secondsInMinute = 60;
+
 chrome.alarms.create("pomodoroTimer", {
-  periodInMinutes: 1 / 60,
+  periodInMinutes: 1 / secondsInMinute,
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
@@ -7,9 +9,21 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.get(["timerInSeconds", "isRunning"], (res) => {
       if (res.isRunning) {
         let timerInSeconds = res.timerInSeconds + 1;
-        console.log(timerInSeconds);
+        let isRunning = true;
+
+        if (timerInSeconds === 25 * secondsInMinute) {
+          this.registration.showNotification("Pomodoro Timer", {
+            body: "25 minutes have passed!",
+            icon: "icon.png",
+          });
+
+          timerInSeconds = 0;
+          isRunning = false;
+        }
+
         chrome.storage.local.set({
           timerInSeconds,
+          isRunning,
         });
       }
     });
